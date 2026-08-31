e6671cfa2919935ca2ae4912aa67e66029ee8c9d
furkansel-ops <selfurkan08@gmail.com>
2026-08-31 13:30:51 +0300

    android: add Capacitor Play Store config

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+const config = {
+  appId: "com.furkansel.yksdefterim",
+  appName: "YKS Defterim",
+  webDir: "dist",
+  android: {
+    backgroundColor: "#EFF2F8"
+  }
+};
+
+export default config;
