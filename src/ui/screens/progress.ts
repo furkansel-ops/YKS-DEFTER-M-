c0ac7e725bc4765b1162875fe95a669729c44197
furkansel-ops <selfurkan08@gmail.com>
2026-09-24 11:30:30 +0300

    perf(ui): keep Statistics loader out of main screen registry

--- src/ui/screens/progress.ts
+++ src/ui/screens/progress.ts
@@ -13,6 +13,5 @@ export const progressScreen:ScreenModule={
     progressLegacyAdapter.render(environment);
     renderAnalysisCenterV43();
     renderProgressDashboard();
-    void import("../progress-v5").then(mod=>{mod.installProgressV5();}).catch(()=>{});
   }
 };
