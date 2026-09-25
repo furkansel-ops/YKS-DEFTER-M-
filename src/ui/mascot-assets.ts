9ca8f916d87007449794a4435f8ebc6376c77301
furkansel-ops <selfurkan08@gmail.com>
2026-09-25 19:54:18 +0300

    Maskot görsellerini public varlıklara bağla

--- src/ui/mascot-assets.ts
+++ src/ui/mascot-assets.ts
@@ -1,14 +1,3 @@
-import notebook from "./mascot-images/notebook";
-import owl from "./mascot-images/owl";
-import cat from "./mascot-images/cat";
-import fox from "./mascot-images/fox";
-import panda from "./mascot-images/panda";
-import rabbit from "./mascot-images/rabbit";
-import turtle from "./mascot-images/turtle";
-import penguin from "./mascot-images/penguin";
-import robot from "./mascot-images/robot";
-import dragon from "./mascot-images/dragon";
-
 export const MASCOT_IDS=["notebook","owl","cat","fox","panda","rabbit","turtle","penguin","robot","dragon"] as const;
 export type MascotId=typeof MASCOT_IDS[number];
 
@@ -19,17 +8,19 @@ export type MascotDefinition={
   image:string;
 };
 
+const image=(id:MascotId)=>`./mascots/${id}.webp`;
+
 export const MASCOTS:readonly MascotDefinition[]=[
-  {id:"notebook",name:"Defter",role:"YKS Defterim'in ana maskotu",image:notebook},
-  {id:"owl",name:"Baykuş",role:"Bilgi ve odak",image:owl},
-  {id:"cat",name:"Kedi",role:"Günlük görevler",image:cat},
-  {id:"fox",name:"Tilki",role:"Hedef ve strateji",image:fox},
-  {id:"panda",name:"Panda",role:"Mola ve motivasyon",image:panda},
-  {id:"rabbit",name:"Tavşan",role:"Seri ve tempo",image:rabbit},
-  {id:"turtle",name:"Kaplumbağa",role:"İstikrar",image:turtle},
-  {id:"penguin",name:"Penguen",role:"Program ve düzen",image:penguin},
-  {id:"robot",name:"Robot",role:"Analiz ve akıllı öneriler",image:robot},
-  {id:"dragon",name:"Ejderha",role:"Başarı ve seviye atlama",image:dragon}
+  {id:"notebook",name:"Defter",role:"YKS Defterim'in ana maskotu",image:image("notebook")},
+  {id:"owl",name:"Baykuş",role:"Bilgi ve odak",image:image("owl")},
+  {id:"cat",name:"Kedi",role:"Günlük görevler",image:image("cat")},
+  {id:"fox",name:"Tilki",role:"Hedef ve strateji",image:image("fox")},
+  {id:"panda",name:"Panda",role:"Mola ve motivasyon",image:image("panda")},
+  {id:"rabbit",name:"Tavşan",role:"Seri ve tempo",image:image("rabbit")},
+  {id:"turtle",name:"Kaplumbağa",role:"İstikrar",image:image("turtle")},
+  {id:"penguin",name:"Penguen",role:"Program ve düzen",image:image("penguin")},
+  {id:"robot",name:"Robot",role:"Analiz ve akıllı öneriler",image:image("robot")},
+  {id:"dragon",name:"Ejderha",role:"Başarı ve seviye atlama",image:image("dragon")}
 ];
 
 export function mascotById(id:string):MascotDefinition{
